b74ab139a773f6031e005779eb5c31e05cbe1f9d
Leonard Pak <leopak2000@gmail.com>
2022-04-08 12:57:25 +0300

    Init task

---
+++ task-2_1/2_1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void ExpSearch(void) {}
+
+size_t BinarySearch(int arr[]) {
+  size_t target_index = 0;
+  return target_index;
+}
+
+int main() {
+  std::cout << "Hello world!" << std::endl;
+
+  return 0;
+}
