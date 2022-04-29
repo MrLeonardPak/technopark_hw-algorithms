71c2578660adfef19ef9e6f3d163b064b8f0bdec
Leonard Pak <leopak2000@gmail.com>
2022-04-29 18:37:30 +0300

    Init task 9(2)_4

---
+++ task-9(2)-4/9(2)-4.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+
+void Run(std::istream& in, std::ostream& out) {}
+
+void TestContest() {
+  {}
+  std::cout << "TestContest: SUCCESS" << std::endl;
+}
+
+int main() {
+  Run(std::cin, std::cout);
+  return 0;
+}
