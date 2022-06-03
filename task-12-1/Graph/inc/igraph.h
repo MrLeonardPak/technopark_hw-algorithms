81bd0ced59e10f268a8f8fb664cb3efeb2be62c1
Leonard Pak <leopak2000@gmail.com>
2022-06-03 16:06:33 +0300

    Add List Graph

---
+++ task-12-1/Graph/inc/igraph.h
@@ -0,0 +1,30 @@
+#ifndef ALGORITHMS_IGRAPH_H
+#define ALGORITHMS_IGRAPH_H
+
+#include <functional>
+#include <vector>
+
+namespace technopark::algorithms::graph {
+
+struct IGraph {
+  virtual ~IGraph() = default;
+
+  // Добавление ребра от from к to.
+  virtual void AddEdge(size_t from, size_t to) = 0;
+
+  virtual int VerticesCount() const = 0;
+
+  virtual std::vector<size_t> GetNextVertices(size_t vertex) const = 0;
+  virtual std::vector<size_t> GetPrevVertices(size_t vertex) const = 0;
+
+  static void DFS(IGraph const& graph,
+                  size_t start_vertex,
+                  std::function<void(size_t)> callback);
+  static void BFS(IGraph const& graph,
+                  size_t start_vertex,
+                  std::function<void(size_t)> callback);
+};
+
+}  // namespace technopark::algorithms::graph
+
+#endif  // ALGORITHMS_IGRAPH_H
